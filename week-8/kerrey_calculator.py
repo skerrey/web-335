2b92dafcd33b0769f8443e0b9bf11432fcb30314
skerrey <sethkerrey@gmail.com>
2022-05-08 15:15:05 -0500

    Create kerrey_calculator.py

---
+++ week-8/kerrey_calculator.py
@@ -0,0 +1,26 @@
+# ============================================
+# Title: Exercise 8.3 - Python in Action
+# File Name: kerrey_calculator.py
+# Author: Professor Krasso
+# Date: 8 May 2022
+# Modified By: Seth Kerrey
+# Description:
+#   Learn basics of Python and how to run in VSCode
+# Resources:
+#   buwebdev, Professor Krasso, Bellevue University
+#   W3Schools Python
+#     https://www.w3schools.com/python/python_functions.asp
+# ===========================================
+
+def add (param1,param2):
+	return param1 + param2
+
+def subtract (param1,param2):
+	return param1 - param2
+
+def divide (param1,param2):
+	return param1/param2
+
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
